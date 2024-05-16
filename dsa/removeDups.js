6ec4bdab3bf3b1f5724fbba595780eaea5338303
Shreya <shreyaray.1206@gmail.com>
2024-05-16 10:13:06 +0530

    Dsa Plus Event Driven Practice

--- dsa/removeDups.js
+++ dsa/removeDups.js
@@ -1,3 +1,27 @@
 function removeDupsNums(arr){
-  const sort
+  const sortArr = arr.sort(a,b => a-b);
+  let newArr =[];
+  for(let i=0; i < sortArr.length; i++){
+    if(sortArr[i] === sortArr[i + 1]){
+        sortArr.splice(i,1)
+    }else {
+      newArr.push(sortArr[i]);
+    }
+  }
+  return newArr;
 }
+
+function removeDupsStr (str){
+    let strArr= str.split(' ').sort();
+    let newStr = [];
+    console.log(strArr);
+    for(let i = 0; i<strArr.length; i++){
+        if(strArr[i] !== strArr[i+1]) {
+            newStr.push(strArr[i]);
+        }
+    }
+    return newStr.join(' ');
+
+}
+console.log(removeDupsStr("zig zag zoh zig zag"));
+
